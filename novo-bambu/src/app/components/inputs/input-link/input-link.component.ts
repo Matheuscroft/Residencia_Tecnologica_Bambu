d2b392099cbcb3747e5df41480d447695625dc63
Matheus OS <matheus.sdoliveira@gmail.com>
2024-10-07 16:32:44 -0300

    ajustes css dados do projeto

--- novo-bambu/src/app/components/inputs/input-link/input-link.component.ts
+++ novo-bambu/src/app/components/inputs/input-link/input-link.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'InputLink',
   templateUrl: './input-link.component.html',
   styleUrls: ['./input-link.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputLinkComponent),
+      multi: true
+    }
+  ]
 })
 
 
@@ -12,4 +20,29 @@ export class InputLinkComponent {
   @Input() text: string = '';
   @Input() keyboardType: string = 'text';
   @Input() autoCapitalize: string = 'none';
+  @Input() maxlength?: number;
+  value: string = '';
+
+  onChange = (value: string) => {};
+  onTouched = () => {};
+
+  updateValue(event: any) {
+    const newValue = event.target.value;
+    this.value = newValue;
+    this.onChange(newValue); 
+  }
+
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
 }
